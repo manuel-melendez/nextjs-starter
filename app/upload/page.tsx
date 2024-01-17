f6b1b249570e48b44f1b682f003e734d1fb4e5b9
Manuel Melendez <mannymelendez1213@gmail.com>
2024-01-17 10:24:52 -0500

    Next auth section

---
+++ app/upload/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+
+  return (
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt="image" />
+      )}
+      <CldUploadWidget
+        uploadPreset="hw3bbzqp"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+          styles: {
+            palette: {
+              window: "#10173a",
+              sourceBg: "#20304b",
+              windowBorder: "#7171D0",
+              tabIcon: "#79F7FF",
+              inactiveTabIcon: "#8E9FBF",
+              menuIcons: "#CCE8FF",
+              link: "#72F1FF",
+              action: "#5333FF",
+              inProgress: "#00ffcc",
+              complete: "#33ff00",
+              error: "#cc3333",
+              textDark: "#000000",
+              textLight: "#ffffff",
+            },
+            fonts: {
+              default: null,
+              "'IBM Plex Sans', sans-serif": {
+                url: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans",
+                active: true,
+              },
+            },
+          },
+        }}
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => {
+          return (
+            <button className="btn btn-primary" onClick={() => open()}>
+              Upload
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
